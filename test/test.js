bf91cb4020ce2d696b1cbd3fa906c5e343b9d3d9
maxbildner <39280058+maxbildner@users.noreply.github.com>
2021-12-02 07:17:59 -0500

    add update testing notes to includde beforeEach hook

--- test/test.js
+++ test/test.js
@@ -19,8 +19,62 @@ const assert = require("assert");
 // Return: ?
 // Notes:
 // - describe is a way to group our tests (easier to read)
-describe("addOne()", () => {
-  it("should add one to input number", () => {
-    assert(addOne(1) === 2, "incorrect!");
+// describe("addOne()", () => {
+//   it("should add one to input number", () => {
+//     assert(addOne(1) === 2, "incorrect!");
+//   });
+// });
+
+
+// https://stackoverflow.com/questions/32660241/mocha-beforeeach-vs-before-execution/32682451
+// 3) ORDER OF FUNCTIONS BEING CALLED:
+// - beforeEach will run before every it callback in it's current scope!! 
+//   but logic inside describe that's not in it block will run before beforeEach!
+// Example 1):
+// beforeEach(() => {
+//   console.log("beforeEach");        // 3  5  7
+// });
+
+// describe("describe", () => {      
+//   console.log("describe inner 1");  // 1
+//   console.log("describe inner 2");  // 2
+  
+
+//   it("it", () => {                
+//     console.log("it inner");        // 4
+//     assert(true);
+//   });
+
+//   it("it", () => {                
+//     console.log("it inner");        // 6
+//     assert(true);
+//   });
+// });
+
+// describe("describe", () => {      
+
+//   it("it", () => {                
+//     console.log("it inner");        // 8
+//     assert(true);
+//   });
+// });
+
+
+// Example 2):
+describe("describe", () => {      
+  beforeEach(() => {
+    console.log("beforeEach");    // 1
+  });
+
+  it("it", () => {                
+    console.log("it inner");      // 2
+    assert(true);
+  });
+});
+
+describe("describe", () => {      
+  it("it", () => {                
+    console.log("it inner");      // 3
+    assert(true);
   });
 });
